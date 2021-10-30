489abf2a24d8565f3b28b8d52d06594cca0956f1
Richard Gieg <richgieg@users.noreply.github.com>
2021-10-30 09:24:08 -0700

    Update dasher.cpp

--- src/dasher.cpp
+++ src/dasher.cpp
@@ -8,10 +8,25 @@ int main() {
 
     InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
 
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{0};
+
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        if (IsKeyPressed(KEY_SPACE)) {
+            velocity -= 10;
+        }
+
+        posY += velocity;
+
+        DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
+
         EndDrawing();
     }
     CloseWindow();
