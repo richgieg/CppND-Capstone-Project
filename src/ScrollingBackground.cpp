145fdead5e1ceca97c2523c8ee209f1e0a9ae81b
Richard Gieg <richgieg@users.noreply.github.com>
2021-10-31 07:50:10 -0700

    Update ScrollingBackground.cpp

--- src/ScrollingBackground.cpp
+++ src/ScrollingBackground.cpp
@@ -2,11 +2,9 @@
 #include "TextureManager.h"
 
 constexpr int farBuildingsScrollSpeed = -25; // pixels per second
-constexpr float farBuildingsScale = 2.0;
 constexpr int backBuildingsScrollSpeed = -50; // pixels per second
-constexpr float backBuildingsScale = 2.0;
 constexpr int foregroundScrollSpeed = -100; // pixels per second
-constexpr float foregroundScale = 2.0;
+constexpr float textureScale = 2.0;
 
 ScrollingBackground::ScrollingBackground():
     farBuildings{textureManager.getTexture("textures/far-buildings.png")},
@@ -18,36 +16,36 @@ ScrollingBackground::ScrollingBackground():
 
 void ScrollingBackground::update(float deltaMs) {
     farBuildingsX += farBuildingsScrollSpeed * deltaMs;
-    if (farBuildingsX <= -farBuildings->texture.width * 2) {
+    if (farBuildingsX <= -farBuildings->texture.width * textureScale) {
         farBuildingsX = 0;
     }
     backBuildingsX += backBuildingsScrollSpeed * deltaMs;
-    if (backBuildingsX <= -backBuildings->texture.width * 2) {
+    if (backBuildingsX <= -backBuildings->texture.width * textureScale) {
         backBuildingsX = 0;
     }
     foregroundX += foregroundScrollSpeed * deltaMs;
-    if (foregroundX <= -foreground->texture.width * 2) {
+    if (foregroundX <= -foreground->texture.width * textureScale) {
         foregroundX = 0;
     }
 }
 
 void ScrollingBackground::draw() {
     Vector2 farBuildingsPosition{farBuildingsX, 0.0};
-    DrawTextureEx(farBuildings->texture, farBuildingsPosition, 0.0, farBuildingsScale, WHITE);
-    farBuildingsPosition.x += farBuildings->texture.width * farBuildingsScale;
-    DrawTextureEx(farBuildings->texture, farBuildingsPosition, 0.0, farBuildingsScale, WHITE);
-    farBuildingsPosition.x += farBuildings->texture.width * farBuildingsScale;
-    DrawTextureEx(farBuildings->texture, farBuildingsPosition, 0.0, farBuildingsScale, WHITE);
+    DrawTextureEx(farBuildings->texture, farBuildingsPosition, 0.0, textureScale, WHITE);
+    farBuildingsPosition.x += farBuildings->texture.width * textureScale;
+    DrawTextureEx(farBuildings->texture, farBuildingsPosition, 0.0, textureScale, WHITE);
+    farBuildingsPosition.x += farBuildings->texture.width * textureScale;
+    DrawTextureEx(farBuildings->texture, farBuildingsPosition, 0.0, textureScale, WHITE);
     Vector2 backBuildingsPosition{backBuildingsX, 0.0};
-    DrawTextureEx(backBuildings->texture, backBuildingsPosition, 0.0, backBuildingsScale, WHITE);
-    backBuildingsPosition.x += backBuildings->texture.width * backBuildingsScale;
-    DrawTextureEx(backBuildings->texture, backBuildingsPosition, 0.0, backBuildingsScale, WHITE);
-    backBuildingsPosition.x += backBuildings->texture.width * backBuildingsScale;
-    DrawTextureEx(backBuildings->texture, backBuildingsPosition, 0.0, backBuildingsScale, WHITE);
+    DrawTextureEx(backBuildings->texture, backBuildingsPosition, 0.0, textureScale, WHITE);
+    backBuildingsPosition.x += backBuildings->texture.width * textureScale;
+    DrawTextureEx(backBuildings->texture, backBuildingsPosition, 0.0, textureScale, WHITE);
+    backBuildingsPosition.x += backBuildings->texture.width * textureScale;
+    DrawTextureEx(backBuildings->texture, backBuildingsPosition, 0.0, textureScale, WHITE);
     Vector2 foregroundPosition{foregroundX, 0.0};
-    DrawTextureEx(foreground->texture, foregroundPosition, 0.0, foregroundScale, WHITE);
-    foregroundPosition.x += foreground->texture.width * foregroundScale;
-    DrawTextureEx(foreground->texture, foregroundPosition, 0.0, foregroundScale, WHITE);
-    foregroundPosition.x += foreground->texture.width * foregroundScale;
-    DrawTextureEx(foreground->texture, foregroundPosition, 0.0, foregroundScale, WHITE);
+    DrawTextureEx(foreground->texture, foregroundPosition, 0.0, textureScale, WHITE);
+    foregroundPosition.x += foreground->texture.width * textureScale;
+    DrawTextureEx(foreground->texture, foregroundPosition, 0.0, textureScale, WHITE);
+    foregroundPosition.x += foreground->texture.width * textureScale;
+    DrawTextureEx(foreground->texture, foregroundPosition, 0.0, textureScale, WHITE);
 }
