9d29d937ff305af296bdaddbb91dcc89fe88bd29
Richard Gieg <richgieg@users.noreply.github.com>
2021-10-31 09:34:52 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ constexpr int windowHeight{380};
 constexpr char windowName[]{"Hipster Hop"};
 
 int main() {
-    SetConfigFlags(FLAG_VSYNC_HINT);
+    SetConfigFlags(FLAG_VSYNC_HINT); // match vertical refresh rate
     InitWindow(windowWidth, windowHeight, windowName);
     Game game;
     while (!WindowShouldClose()) {
