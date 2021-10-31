feaf4b35b6fbbf01a043271777a82139e1cfc8cc
Richard Gieg <richgieg@users.noreply.github.com>
2021-10-30 23:01:27 -0700

    Update Player.cpp

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,7 +1,5 @@
 #include "Player.h"
 
-constexpr float velocityX{500.0}; // pixels per second
-
 Player::Player(): Entity{"textures/player.png", 1, 6, 6} {
     setX(GetScreenWidth() / 3.0 - getWidth() / 2.0);
     setActive(true);
