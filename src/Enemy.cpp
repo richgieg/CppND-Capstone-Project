93e160ed892e8b5b655042aba777644bbd381e51
Richard Gieg <richgieg@users.noreply.github.com>
2021-10-30 20:42:58 -0700

    Update Enemy.cpp

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,3 +1,5 @@
 #include "Enemy.h"
 
-Enemy::Enemy(): Entity{"textures/enemy.png", 8, 8, 3} {}
+Enemy::Enemy(): Entity{"textures/enemy.png", 8, 8, 3} {
+    setX(GetScreenWidth()); // start off screen
+}
