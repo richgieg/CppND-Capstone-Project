7dd575b1ff63b29cae3a62aea7e1408cd6bd970b
Richard Gieg <richgieg@users.noreply.github.com>
2021-10-31 08:15:12 -0700

    Update EnemySpawner.cpp

--- src/EnemySpawner.cpp
+++ src/EnemySpawner.cpp
@@ -7,6 +7,7 @@ constexpr float spawnSeconds{2.0};
 // After five spawns, the number of enemies per spawn
 // increases by one.
 constexpr int spawnsPerLevel{5};
+constexpr float enemyVelocityX{-1000}; // pixels per second
 
 EnemySpawner::EnemySpawner(EntityPool<Enemy> *enemies):
     enemies{enemies},
@@ -23,7 +24,7 @@ void EnemySpawner::update(float deltaSeconds) {
             if (enemyIterator != enemies->end()) {
                 auto x = GetScreenWidth() + (i * enemyIterator->getWidth());
                 enemyIterator->setX(x);
-                enemyIterator->setVelocityX(-1000);
+                enemyIterator->setVelocityX(enemyVelocityX);
                 enemyIterator->setActive(true);
             }
         }
