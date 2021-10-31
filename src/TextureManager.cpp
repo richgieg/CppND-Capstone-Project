8bf6c6e1cccc49bac16116b72e4630c83cfa0200
Richard Gieg <richgieg@users.noreply.github.com>
2021-10-30 19:59:56 -0700

    Update TextureManager.cpp

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -2,7 +2,7 @@
 #include "raylib.h"
 #include "TextureManager.h"
 
-static TextureManager::TextureManager& getInstance() {
+TextureManager& TextureManager::getInstance() {
     static TextureManager instance; // Instantiated on first use.
     return instance;
 }
@@ -10,10 +10,8 @@ static TextureManager::TextureManager& getInstance() {
 std::shared_ptr<Texture2DHandle> TextureManager::getTexture(std::string file) {
     auto iterator = textureHandleMap.find(file);
     if (iterator != textureHandleMap.end()) { // already in map
-        std::cout << "already in map: " << file << "\n";
         return iterator->second;
     } else { // load and add to map
-        std::cout << "adding to map: " << file << "\n";
         auto handle = std::make_shared<Texture2DHandle>(file);
         textureHandleMap.emplace(file, handle);
         return handle;
